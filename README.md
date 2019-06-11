26cc4831a5f77c7805c6f4dfd3346c4458cb0036
masdzub <masdzub@gmail.com>
2019-06-11 22:47:07 +0700

    modified status battery

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # low-bat-notification
 
-you can use this script on cronjob every 3 or 5  minutes
+you can use this script on cronjob every 1 to 5  minutes
+
+
+```* * * * * bash /home/masdzub/.config/batinfo.sh```
--- batinfo.sh
+++ batinfo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 BATTINFO=`acpi -b`
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:15:00 ]] ; then
-    DISPLAY=:0.0 /usr/bin/notify-send "low battery" "$BATTINFO"
+if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:20:00 ]] ; then
+    DISPLAY=:0.0 /usr/bin/notify-send -u critical "Low Battery" "Status $BATTINFO"
 fi
